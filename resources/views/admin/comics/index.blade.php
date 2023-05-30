1d9c6fde9ebbdc6c505454e4d7809e3cdf52928d
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-05-30 19:40:30 +0200

    add bonus

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -38,11 +38,49 @@
                     <td class="">
                         <a class="btn btn-primary" href="{{route('comics.show',$comic)}}" role="button">VIEW</a>
                         <a class="btn btn-primary" href="{{route('comics.edit',$comic)}}" role="button">EDIT</a>
-                        <form action="{{route('comics.destroy',$comic->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">DELETE</button>
-                        </form>
+
+                        <!-- Modal trigger button -->
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{$comic->id}}">
+                            Delete
+                        </button>
+
+                        <!-- Modal Body -->
+                        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                        <div class="modal fade" id="modal-{{$comic->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modal-{{$comic->id}}" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modal-{{$comic->id}}">Delete {{$comic->title}} </h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        are you very very very very very very very very very very very sure?
+                                    </div>
+                                    <div class="modal-footer">
+
+                                        <form action="{{route('comics.destroy',$comic->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">yes!</button>
+                                        </form>
+
+                                        <button type="button" class="btn btn-secondary " data-bs-dismiss="modal">Nop!</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <!-- Optional: Place to the bottom of scripts -->
+                        <script>
+                            const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
+                        </script>
+
+
+
+
+
+
                     </td>
                 </tr>
                 @empty
